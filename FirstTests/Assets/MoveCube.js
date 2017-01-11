48dfec84a4ab897ec1096e186f712b9744b7458d
chrisvtan <chrisvtan@gmail.com>
2017-01-11 12:34:17 -0500

    Added First Tests

---
+++ FirstTests/Assets/MoveCube.js
@@ -0,0 +1,39 @@
+﻿#pragma strict
+
+
+var rb: Rigidbody;
+var grounded = false;
+
+public var JumpForce = 500.0f;
+
+function Start() {
+  rb = GetComponent. < Rigidbody > ();
+}
+
+function Update() {
+  //moving the cube around
+  var moveVertical: float = Input.GetAxis("Vertical");
+  var moveHorizontal: float = Input.GetAxis("Horizontal");
+  transform.position.z += moveVertical;
+  transform.position.x += moveHorizontal;
+  //cube jumpswhen space is pressed && touching something
+  if (Input.GetKeyDown("space")&&grounded) {
+
+
+    rb.AddForce(Vector3(0, JumpForce, 0));
+  }
+}
+
+
+function onCollisionEnter(colliosion: Collision) {
+
+  if (collision.collider.tag =="ground") {
+    grounded = true;
+  }
+}
+
+
+function onCollisionExit() {
+
+  grounded = false;
+}
---
+++ FirstTests/Assets/TurnLampOff.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var myLight:Light;
+public var fadeOutSpeed:float = .01;
+function Start () {
+
+	myLight = GetComponent.<Light>();
+
+}
+
+function Update () {
+	if(Input.GetKeyDown("space")==true){
+
+		if(myLight.enabled==false){
+		myLight.enabled=true;
+		}
+		else{
+		myLight.enabled=false;
+		}
+
+
+	}
+
+}
